a2554e3ac648489f03c7efa1a780b5bacca0c120
ambarytl <44284655+ambarytl@users.noreply.github.com>
2019-04-01 19:26:12 +0530

    foo

--- main.js
+++ main.js
@@ -1,57 +1,53 @@
+var db = firebase.firestore();
+
 //Initialize Sidebar
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
 
-const  postTemplate="<div id=post>";
+const postTemplate = "<div id=post>";
 
 
-firebase.initializeApp({
-    apiKey: 'AIzaSyDohZMBbT1TVeDf-zu1B0S3tMXvxbgiL94',
-    authDomain: 'aviral-vlogs.firebaseapp.com',
-    projectId: 'aviral-vlogs'
-  });
-  
-  var db = firebase.firestore();
+// firebase.initializeApp({
+//     apiKey: 'AIzaSyDohZMBbT1TVeDf-zu1B0S3tMXvxbgiL94',
+//     authDomain: 'aviral-vlogs.firebaseapp.com',
+//     projectId: 'aviral-vlogs'
+// });
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     $('#summernote').summernote();
-  });
+});
 
-var data=[];
+var data = [];
 
 
-function post(ih,ts)
-{
+function post(ih, ts) {
     this.ih;
-    this.ts=ts;
+    this.ts = ts;
 
 }
 
 
-function updateUI()
-{
+function updateUI() {
 
 }
 
-function fetchData()
-{
-    db.collection("adminposts").get().then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-           
+function fetchData() {
+    db.collection("adminposts").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+
             data.push(doc.data());
         });
     });
 }
 
 
-function postData()
-{
+function postData() {
     var markupStr = $('#summernote').summernote('code');
-        console.log(markupStr);
-        db.collection("new").add({html:markupStr,time:firebase.firestore.FieldValue.serverTimeStamp()}).then(function(docRef)
-                                                                                                     {
-                                                                                console.log("Document written with ID: ", docRef.id);
-                                                                                                        });
+    console.log(markupStr);
+    db.collection("new").add({ html: markupStr, time: firebase.firestore.FieldValue.serverTimestamp() }).then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    });
 }
