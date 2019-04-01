e61e7ef337ceb15b096d4c7672ead4f1ec302e05
Gagan <gagans@iitbhilai.ac.in>
2019-04-01 19:13:19 +0530

    cgdg

--- main.js
+++ main.js
@@ -3,3 +3,55 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+
+const  postTemplate="<div id=post>";
+
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyDohZMBbT1TVeDf-zu1B0S3tMXvxbgiL94',
+    authDomain: 'aviral-vlogs.firebaseapp.com',
+    projectId: 'aviral-vlogs'
+  });
+  
+  var db = firebase.firestore();
+
+$(document).ready(function() {
+    $('#summernote').summernote();
+  });
+
+var data=[];
+
+
+function post(ih,ts)
+{
+    this.ih;
+    this.ts=ts;
+
+}
+
+
+function updateUI()
+{
+
+}
+
+function fetchData()
+{
+    db.collection("adminposts").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+           
+            data.push(doc.data());
+        });
+    });
+}
+
+
+function postData()
+{
+    var markupStr = $('#summernote').summernote('code');
+        console.log(markupStr);
+        db.collection("new").add({html:markupStr,time:firebase.firestore.FieldValue.serverTimeStamp()}).then(function(docRef)
+                                                                                                     {
+                                                                                console.log("Document written with ID: ", docRef.id);
+                                                                                                        });
+}
