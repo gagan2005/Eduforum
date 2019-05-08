48fc9c7abec420169d4d2d430b34de925034980a
ambarytl <ambar.ytl@gmail.com>
2019-05-08 17:39:20 +0530

    add sample practice sheet

---
+++ practice-sheets.js
@@ -0,0 +1,5 @@
+//Initialize Sidebar
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
